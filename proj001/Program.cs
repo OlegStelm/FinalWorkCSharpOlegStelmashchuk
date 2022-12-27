a5f6009e12c94e903b49cfee369a51a4c3bc1a79
Oleg.Stelm <oleg.stelm@gmail.com>
2022-12-28 02:10:40 +0300

    AddProj

--- proj001/Program.cs
+++ proj001/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*  Задача:
+Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → []
+*/
+
+int m;
+Console.WriteLine("Enter the number of elements: ");
+int.TryParse(Console.ReadLine()!, out m);
+string[] arrayStart = new string[m];
+for (int i = 0; i < arrayStart.Length; i++)
+{
+    Console.WriteLine("Enter an element: ");
+    arrayStart[i] = Console.ReadLine()!;
+}
+Console.Write($"[");
+for (int i = 0; i < arrayStart.Length; i++)
+{
+    if (i == arrayStart.Length - 1)
+    {
+        Console.Write($"{arrayStart[i]}");
+    }
+    else
+    {
+        Console.Write($"{arrayStart[i]}*");
+    }
+}
+Console.WriteLine($"]");
